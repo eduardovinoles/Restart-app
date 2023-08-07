bddee0c23ee7bab91b397580fe624ae7f5322373
Eduardo <evinoles@bootia.dev>
2023-08-07 12:52:30 -0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by eduardo vinoles on 7/8/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
